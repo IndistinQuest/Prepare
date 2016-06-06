84a2f8cbc850e16f57e0893b05b79a62ea4dd4e8
Hatanas-SUB <hatanas726@gmail.com>
2016-06-06 20:00:18 +0900

    update Drawable
    コメントを追加

--- Prepare/DrawableList.h
+++ Prepare/DrawableList.h
@@ -10,10 +10,26 @@ class DrawableList
 private:
     std::multimap<int, std::shared_ptr<Drawable>> m_drawables;
 public:
+    /// <summary>
+    /// コンストラクタ
+    /// </summary>
     DrawableList() = default;
+
+    /// <summary>
+    /// デストラクタ
+    /// </summary>
     ~DrawableList();
+
 public:
+    /// <summary>
+    /// 描画リストに追加する
+    /// </summary>
+    /// <param name="drawable"> 描画するオブジェクト <param>
     void add(std::shared_ptr<Drawable> drawable);
+
+    /// <summary>
+    /// 全てのオブジェクトを描画する
+    /// </summary>
     void drawAll() const;
 };
 
